889772e9efb60d5576fb236779e01f80ec73d6a2
Armin-FalDiS <armin.fallah96@gmail.com>
2023-12-11 11:19:09 +0330

    Implement google drive download + improvements

--- tgbot.py
+++ tgbot.py
@@ -1,11 +1,21 @@
+from functools import partial
+import http
 from yt_dlp import YoutubeDL
 import logging
 import os
 import subprocess
 import re
+from urllib.parse import quote
 from pathlib import Path
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler
+from telegram.constants import FileSizeLimit
+import gdown
+import asyncio
+
+# regexes
+gdrive_file = r'https?:\/\/drive\.google\.com\/(?:u\/\d+\/uc\?id=|file\/d\/|uc\?id=)[a-zA-Z0-9-_]+'
+gdrive_folder = r'https?:\/\/drive\.google\.com\/drive\/folders\/[a-zA-Z0-9-_]+'
 
 # set up logger
 logging.basicConfig(
@@ -115,167 +125,228 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.send_message(update.effective_chat.id, 'Yo ! Hit me up with da URL and I will get right on it b-word !!')
 
 
+async def check_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.effective_chat or not update.effective_user or not_vip(update.effective_user.id):
+        return
+
+    msg = update.message.text
+
+    if not msg.startswith('https://'):
+        update.message.reply_text("Just links bro")
+    elif re.match(gdrive_file, msg):
+        await download_file(update, context)
+    elif re.match(gdrive_folder, msg):
+        await download_folder(update, context)
+    else:
+        await check_vid(update, context)
+
+
 async def check_vid(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Handler returning available qualities of the inputed youtube URL
     allowing the user to choose using buttons
 
     Won't return qualities that have no audio
     """
-    if update.effective_chat and update.effective_user:
-        if not_vip(update.effective_user.id):
+
+    ans = await update.message.reply_text('Wait for it...')
+
+    # fetch formats
+    with YoutubeDL() as ydl:
+        formats = []
+        try:
+            a = ydl.extract_info(update.message.text, download=False)
+            formats = a.get('formats')
+        except Exception as err:
+            await ans.edit_text('That was a dumb thing to send me. Failure was ... inevitable')
+            await update.effective_chat.send_message('But since you indulged little old me, here is why: ')
+            await update.effective_chat.send_message(str(err))
             return
+        # empty formats is wierd (since the error handler should catch it)
+        if len(formats) == 0:
+            await ans.edit_text('Jesus horatio! I dunno what happened with that one !!')
+            return
+        # create the buttons for user
+        keyboard = []
+        options = []
 
-        ans = await update.message.reply_text('Wait for it...')
+        # add music (audio) option
+        label = '~ Music / Audio ~'
+        options.append(['bestaudio[ext!=webm]', update.message.text, False])
+        keyboard.append([InlineKeyboardButton(
+            label, callback_data=len(options) - 1)])
 
-        # fetch formats
-        with YoutubeDL() as ydl:
-            formats = []
-            try:
-                a = ydl.extract_info(update.message.text, download=False)
-                formats = a.get('formats')
-            except Exception as err:
-                await ans.edit_text('That was a dumb thing to send me. Failure was ... inevitable')
-                await update.effective_chat.send_message('But since you indulged little old me, here is why: ')
-                await update.effective_chat.send_message(str(err))
-                return
-            # empty formats is wierd (since the error handler should catch it)
-            if len(formats) == 0:
-                await ans.edit_text('Jesus horatio! I dunno what happened with that one !!')
-                return
-            # create the buttons for user
-            keyboard = []
-            options = []
-
-            # add music (audio) option
-            label = '~ Music / Audio ~'
-            options.append(['bestaudio[ext!=webm]', update.message.text, False])
+        for f in formats:
+            # skip if the file seems to contain no audio (attempt to skip non audible options)
+            if 'acodec' in f and f['acodec'] == 'none':
+                continue
+            # skip unwanted extensions
+            if ('ext' in f and f['ext'] == 'webm'):
+                continue
+            # skip audio files
+            if ('height' in f and f['height'] == 'None') or ('resolution' in f and f['resolution'] == 'audio only') or ('audio_ext' in f and f['audio_ext'] != 'none'):
+                continue
+            # grab format identifier (used for download)
+            id = f['format_id']
+            # extract format (used for button label)
+            # format = id
+            # if 'format' in f:
+            #     format = f['format']
+            # elif 'format_note' in f:
+            #     format = f['format_note']
+            # # extract size if available
+            # size = 0
+            # if 'filesize' in f:
+            #     size = f['filesize']
+            # elif 'filesize_approx' in f:
+            #     size = f['filesize_approx']
+            # if size == 0:
+            #     size = '??'
+            # else:
+            #     try:
+            #         size = '{:0.2f}'.format(int(size) * 0.000001)
+            #     except:
+            #         size = '??'
+            ext = f['ext']
+            # keyboard button label
+            # label = '{} - .{} ({} MB)'.format(format, ext, size)
+            label = '{}.{}'.format(f['format_note'], ext)
+            # keep request in ram
+            options.append([id, update.message.text, True])
+            # buttons carry the id given by system for the user
             keyboard.append([InlineKeyboardButton(
                 label, callback_data=len(options) - 1)])
 
-            for f in formats:
-                # skip if the file seems to contain no audio (attempt to skip non audible options)
-                if 'acodec' in f and f['acodec'] == 'none':
-                    continue
-                # skip unwanted extensions
-                if ('ext' in f and f['ext'] == 'webm'):
-                    continue
-                # skip audio files
-                if ('height' in f and f['height'] == 'None') or ('resolution' in f and f['resolution'] == 'audio only') or ('audio_ext' in f and f['audio_ext'] != 'none'):
-                    continue
-                # grab format identifier (used for download)
-                id = f['format_id']
-                # extract format (used for button label)
-                # format = id
-                # if 'format' in f:
-                #     format = f['format']
-                # elif 'format_note' in f:
-                #     format = f['format_note']
-                # # extract size if available
-                # size = 0
-                # if 'filesize' in f:
-                #     size = f['filesize']
-                # elif 'filesize_approx' in f:
-                #     size = f['filesize_approx']
-                # if size == 0:
-                #     size = '??'
-                # else:
-                #     try:
-                #         size = '{:0.2f}'.format(int(size) * 0.000001)
-                #     except:
-                #         size = '??'
-                ext = f['ext']
-                # keyboard button label
-                # label = '{} - .{} ({} MB)'.format(format, ext, size)
-                label = '{}.{}'.format(f['format_note'], ext)
-                # keep request in ram
-                options.append([id, update.message.text, True])
-                # buttons carry the id given by system for the user
-                keyboard.append([InlineKeyboardButton(
-                    label, callback_data=len(options) - 1)])
-
-            # keep options for the user (each user can only have one active request)
-            requests[update.effective_user.id] = options
-            # prompt user to choose the quality
-            await ans.edit_text('What be the Quality G ?', reply_markup=InlineKeyboardMarkup(keyboard))
+        # keep options for the user (each user can only have one active request)
+        requests[update.effective_user.id] = options
+        # prompt user to choose the quality
+        await ans.edit_text('What be the Quality G ?', reply_markup=InlineKeyboardMarkup(keyboard))
 
 
 async def download_vid(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Handler in charge of saving the requested file to the disk
     and sending it to the user
     """
-    if update.effective_chat and update.effective_user:
-        if not_vip(update.effective_user.id):
-            return
-        query = update.callback_query
 
-        await query.answer('Checking...')
+    query = update.callback_query
+    await query.answer('Checking...')
 
-        try:
-            # fetch the file's info from request container of the user using the index
-            [id, url, is_video] = requests.pop(update.effective_user.id)[
-                int(query.data)]
-        except:
-            await query.edit_message_text('Unacceptable')
-            return
+    try:
+        # fetch the file's info from request container of the user using the index
+        [id, url, is_video] = requests.pop(update.effective_user.id)[
+            int(query.data)]
+    except:
+        await query.edit_message_text('Unacceptable')
+        return
 
-        await query.edit_message_text('Downloading...')
+    await query.edit_message_text('Downloading...')
 
-        # download the file grab the filename
-        filename = ''
-        options = {
-            'outtmpl': out_dir + '%(title)s - %(format_note)s.%(ext)s',
-            'format': id,
-            'nocheckcertificate': True
-        }
-        with YoutubeDL(options) as ytdl:
-            info = ytdl.extract_info(url, download=True)
-            filename = ytdl.prepare_filename(info)
+    # download the file grab the filename
+    filename = ''
+    options = {
+        'outtmpl': out_dir + '%(title)s - %(format_note)s.%(ext)s',
+        'format': id,
+        'nocheckcertificate': True
+    }
+    with YoutubeDL(options) as ytdl:
+        info = ytdl.extract_info(url, download=True)
+        filename = ytdl.prepare_filename(info)
 
-        # no filename no file
-        if filename == '':
-            await query.edit_message_text('Error proccessing your request :(')
+    # no filename no file
+    if filename == '':
+        await query.edit_message_text('Error proccessing your request :(')
 
-        # new file name replacing unwanted chars with underscore
-        fn = re.sub('[#,\',"]', '', filename)
-        fn = re.sub('\s', '_', fn)
+    # new file name replacing unwanted chars with underscore
+    fn = re.sub('[#,\',"]', '', filename)
+    fn = re.sub('\s', '_', fn)
 
-        # rename file
-        os.rename(filename, fn)
+    # rename file
+    os.rename(filename, fn)
 
-        # extract duration
-        output = subprocess.run(['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries',
-                                'stream=duration', '-of', 'default=noprint_wrappers=1:nokey=1', fn], stdout=subprocess.PIPE)
-        duration = 0
-        try:
-            duration = int(float(output.stdout.decode('ascii')))
-        except:
-            pass
+    # extract duration
+    output = subprocess.run(['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries',
+                            'stream=duration', '-of', 'default=noprint_wrappers=1:nokey=1', fn], stdout=subprocess.PIPE)
+    duration = 0
+    try:
+        duration = int(float(output.stdout.decode('ascii')))
+    except:
+        pass
 
-        # Telegram has a limit of 50 MB for bot upload
-        if os.path.getsize(fn) > 50000000:
-            # prompt user to download the file from server
-            await query.edit_message_text('That file is way too big for me to fit it through your small tiny hole. Go get it yourself: \n\n' + dl_url + Path(fn).name)
+    # Telegram has a limit of 50 MB for bot upload
+    if os.path.getsize(fn) > FileSizeLimit.FILESIZE_UPLOAD:
+        # prompt user to download the file from server
+        await query.edit_message_text('That file is way too big for me to fit it through your small tiny hole. Go get it yourself: \n\n' + dl_url + Path(fn).name)
+    else:
+        # upload file to telegram
+        await query.edit_message_text(text='Uploading to telegram...')
+
+        if is_video:
+            # extract thumbnail
+            thumbnail_fn = fn[:fn.rfind('.')] + '.jpg'
+            subprocess.run(['ffmpeg', '-i', fn, '-ss', '1', '-vframes', '1',
+                            thumbnail_fn], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+            # send file as video
+            await context.bot.send_video(update.effective_chat.id, open(fn, 'rb'), duration, thumbnail=open(thumbnail_fn, 'rb'))
+
+            # clean up thumbnail file
+            os.remove(thumbnail_fn)
         else:
-            # upload file to telegram
-            await query.edit_message_text(text='Uploading to telegram...')
+            # send file as audio
+            await context.bot.send_audio(update.effective_chat.id, open(fn, 'rb'), duration=duration)
 
-            if is_video:
-                # extract thumbnail
-                thumbnail_fn = fn[:fn.rfind('.')] + '.jpg'
-                subprocess.run(['ffmpeg', '-i', fn, '-ss', '1', '-vframes', '1',
-                               thumbnail_fn], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # clean up message
+        await query.delete_message()
 
-                # send file as video
-                await context.bot.send_video(update.effective_chat.id, open(fn, 'rb'), duration=duration, thumb=open(thumbnail_fn, 'rb'))
+    if update.message:
+        update.message.delete()
 
-                # clean up thumbnail file
-                os.remove(thumbnail_fn)
-            else:
-                # send file as audio
-                await context.bot.send_audio(update.effective_chat.id, open(fn, 'rb'), duration=duration)
 
-            # clean up message
-            await query.delete_message()
+async def download_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ans = await update.message.reply_text('Downloading...')
+
+    # Try to get the running loop
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        # If no running loop, create a new one
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    fn = await asyncio.get_running_loop().run_in_executor(None, partial(gdown.download, url=update.message.text, output=out_dir, quiet=True, resume=True))
+
+    # Telegram has a limit of 50 MB for bot upload
+    if os.path.getsize(fn) > FileSizeLimit.FILESIZE_UPLOAD:
+        # prompt user to download the file from server
+        await ans.edit_message_text('That file is way too big for me to fit it through your small tiny hole. Go get it yourself: \n\n' + quote(dl_url + fn))
+    else:
+        # upload file to telegram
+        await ans.edit_message_text(text='Uploading to telegram...')
+        await context.bot.send_document(update.effective_chat.id, open(os.path.join(out_dir, fn), 'rb'))
+
+        # clean up message
+        await ans.delete_message()
+
+    if update.message:
+        update.message.delete()
+
+
+async def download_folder(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ans = await update.message.reply_text('Downloading...')
+
+    # Try to get the running loop
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        # If no running loop, create a new one
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    files = await asyncio.get_running_loop().run_in_executor(None, partial(gdown.download_folder, url=update.message.text, output=out_dir, quiet=True))
+
+    await ans.edit_message_text('Yo big boy lots of files here. Go get them yourself: \n\n' + '\n'.join([f'{dl_url}{quote(file)}' for file in files]))
+
+    if update.message:
+        update.message.delete()
 
 
 async def cleanup(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -299,12 +370,16 @@ else:
     update_vip_list()
 
 app = ApplicationBuilder().token(api_token).build()
+
+# command handlers
 app.add_handler(CommandHandler('start', start))
 app.add_handler(CommandHandler('cleanup', cleanup))
+
 # password is checked using Text filter
-app.add_handler(MessageHandler(filters.Text(
-    [super_secret_password]) & (~filters.COMMAND), vip_maker))
-app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), check_vid))
+app.add_handler(MessageHandler(filters.Text([super_secret_password]) & (~filters.COMMAND), vip_maker))
+
+# main logic handlers
+app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), check_link))
 app.add_handler(CallbackQueryHandler(download_vid))
 
 # start the webhook
